da3ad2363d21f529b719223a595d84c2e904265d
mikigal <mikigal.priv@gmail.com>
2021-05-06 14:06:48 +0200

    Added Parallels Desktop Launcher and Adobe patch

---
+++ Resources/Adobe patch.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+echo "Staring patching..."
+
+for file in MMXCore FastCore TextModel libiomp5.dylib libtbb.dylib libtbbmalloc.dylib; do
+    find /Applications/Adobe* -type f -name $file | while read -r FILE; do
+        sudo -v
+        echo "found $FILE"
+        [[ ! -f ${FILE}.back ]] && sudo cp -f $FILE ${FILE}.back || sudo cp -f ${FILE}.back $FILE
+        echo $FILE | grep libiomp5 >/dev/null
+        if [[ $? == 0 ]]; then
+            dir=$(dirname "$FILE")
+            [[ ! -f ${HOME}/libiomp5.dylib ]] && cd $HOME && curl -sO https://excellmedia.dl.sourceforge.net/project/badgui2/libs/mac64/libiomp5.dylib
+            echo -n "replacing " && sudo cp -vf ${HOME}/libiomp5.dylib $dir && echo
+            rm -f ${HOME}/libiomp5.dylib
+            continue
+        fi
+        echo $FILE | grep TextModel >/dev/null
+        [[ $? == 0 ]] && echo "emptying $FILE" && sudo echo -n >$FILE && continue
+        echo "patching $FILE \n"
+        sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x6A\x00|\x90\x90\x90\x90\x56\xE8\x3A\x00|sg' $FILE
+        sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x4A\x00|\x90\x90\x90\x90\x56\xE8\x1A\x00|sg' $FILE
+    done
+done
+
+echo "Generating LaunchAgent for MKL_DEBUG_CPU_TYPE"
+
+[ ! -d $HOME/Library/LaunchAgents ] && mkdir $HOME/Library/LaunchAgents
+AGENT=$HOME/Library/LaunchAgents/environment.plist
+sysctl -n machdep.cpu.brand_string | grep FX >/dev/null 2>&1
+x=$(echo $(($? != 0 ? 5 : 4)))
+cat >$AGENT <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+ <key>Label</key>
+ <string>mkl-debug</string>
+ <key>ProgramArguments</key>
+ <array>
+ <string>sh</string>
+ <string>-c</string>
+    <string>launchctl setenv MKL_DEBUG_CPU_TYPE $x;</string>
+ </array>
+ <key>RunAtLoad</key>
+ <true/>
+</dict>
+</plist>
+EOF
+launchctl load ${AGENT} >/dev/null 2>&1
+launchctl start ${AGENT} >/dev/null 2>&1
+
+echo "Removing Deep_Font plugin"
+sudo find / -name "Deep_Font" | xargs rm;
+
+echo "Patching done, reboot your Hackintosh"
