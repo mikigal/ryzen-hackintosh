4cd05edb22e66f2e179d88c4198924ac1393399c
mikigal <mikigal.priv@gmail.com>
2021-05-19 20:12:13 +0200

    Reworked Ryzen patches

--- Resources/adobe_patch.sh
+++ Resources/adobe_patch.sh
@@ -23,34 +23,7 @@ for file in MMXCore FastCore TextModel libiomp5.dylib libtbb.dylib libtbbmalloc.
     done
 done
 
-echo "Generating LaunchAgent for MKL_DEBUG_CPU_TYPE"
-
-[ ! -d $HOME/Library/LaunchAgents ] && mkdir $HOME/Library/LaunchAgents
-AGENT=$HOME/Library/LaunchAgents/environment.plist
-sysctl -n machdep.cpu.brand_string | grep FX >/dev/null 2>&1
-x=$(echo $(($? != 0 ? 5 : 4)))
-cat >$AGENT <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
- <key>Label</key>
- <string>mkl-debug</string>
- <key>ProgramArguments</key>
- <array>
- <string>sh</string>
- <string>-c</string>
-    <string>launchctl setenv MKL_DEBUG_CPU_TYPE $x;</string>
- </array>
- <key>RunAtLoad</key>
- <true/>
-</dict>
-</plist>
-EOF
-launchctl load ${AGENT} >/dev/null 2>&1
-launchctl start ${AGENT} >/dev/null 2>&1
-
 echo "Removing Deep_Font plugin"
-sudo find / -name "Deep_Font" | xargs rm;
+sudo find /Applications/Adobe* -name "Deep_Font" | xargs sudo rm;
 
-echo "Patching done, reboot your Hackintosh"
+echo "Adobe Patching done, remember to run Ryzen_patch.sh too"
---
+++ Resources/ryzen_patch.sh
@@ -0,0 +1,30 @@
+test -f $HOME/Library/LaunchAgents/environment.plist && echo "LaunchAgent already exists! You don't have to patch it again" && exit
+
+echo "Generating LaunchAgent for MKL_DEBUG_CPU_TYPE"
+
+[ ! -d $HOME/Library/LaunchAgents ] && mkdir $HOME/Library/LaunchAgents
+AGENT=$HOME/Library/LaunchAgents/environment.plist
+sysctl -n machdep.cpu.brand_string | grep FX >/dev/null 2>&1
+x=$(echo $(($? != 0 ? 5 : 4)))
+cat >$AGENT <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+ <key>Label</key>
+ <string>mkl-debug</string>
+ <key>ProgramArguments</key>
+ <array>
+ <string>sh</string>
+ <string>-c</string>
+    <string>launchctl setenv MKL_DEBUG_CPU_TYPE $x;</string>
+ </array>
+ <key>RunAtLoad</key>
+ <true/>
+</dict>
+</plist>
+EOF
+launchctl load ${AGENT} >/dev/null 2>&1
+launchctl start ${AGENT} >/dev/null 2>&1
+
+echo "Created LaunchAgent, reboot your Hackintosh"
